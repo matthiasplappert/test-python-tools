fee28e4af885aa8a4bf15c7dc116b59a46447f5f
Virginia Peón García <virginia.peon.garcia@gmail.com>
2019-11-23 13:42:43 +0100

    first commit

---
+++ test_mymodule.py
@@ -0,0 +1,12 @@
+from mymodule import get_ip
+
+def test_get_ip_type():
+    """The IP is expected to be a string."""
+    ip = get_ip()
+    assert isinstance(ip, str)
+
+
+def test_get_ip_dots():
+    """The IP(v4) is expected to have 4 dots."""
+    ip = get_ip()
+    assert ip.count('.') == 3
