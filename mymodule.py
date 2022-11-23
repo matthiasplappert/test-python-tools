d73d42893a76d848b77d61fafefb180276f5c370
Matthias Plappert <matthiasplappert@github.com>
2022-11-23 14:37:28 +0100

    Always use IPv4

--- mymodule.py
+++ mymodule.py
@@ -3,4 +3,4 @@ import requests
 
 def get_ip():
     """Get my current external IP."""
-    return requests.get("https://icanhazip.com").text.strip()
+    return requests.get("https://ipv4.icanhazip.com/").text.strip()
